abfcc3fdaa1a234f242c3512f88dc5cd6cae3414
Aktan3020 <116088191+Aktan3020@users.noreply.github.com>
2022-11-12 10:06:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-// function stepen(a,b){
-//     return a**b
-// }
-// let a = 4;
-// let b = 2;
 
-// console.log(stepen(a,b))
+let textcont = 0;
+var par = document.querySelector("p")
 
-// let mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-// let mass2 = [];
-
-// for (let i = 0; i <= mass.length; i++) {
-//     let f = mass[i] % 2;
-//     if (f == 0){
-//        mass2.push(mass[i])
-//     }
-// }
-// console.log(mass2)
-
-
-// !!!Чтобы найти среднее арифметическое, нужно сложить все числа и поделить их сумму на их количество.
-
-function credne(a,b){
-    return (a+b)/2
+function plus() { 
+let newf = +par.textContent + 1;
+par.textContent = newf;
 }
-let a = 4;
-let b = 2;
 
-console.log(credne(a,b))
+function minus() {
+
+    let newf = +par.textContent - 1;
+    par.textContent = newf;  
+    if (newf < 0){
+         par.textContent = 0;
+         alert("Минимальное число 0")
+        
+    }
+
+
+}
+let btn1 = document.querySelector(".ee");
+let btn2 = document.querySelector(".e");
+let result = par.textContent = textcont;
+btn1.addEventListener('click', plus);
+btn2.addEventListener('click', minus);
